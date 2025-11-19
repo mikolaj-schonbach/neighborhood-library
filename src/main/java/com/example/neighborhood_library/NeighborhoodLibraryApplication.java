9b86782589e18a4b0b973013432eb6abdc6f136b
Mikołaj Schonbach <mikolaj.schonbach@gmail.com>
2025-11-19 13:04:15 +0100

    chore: initial commit

---
+++ src/main/java/com/example/neighborhood_library/NeighborhoodLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.neighborhood_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborhoodLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighborhoodLibraryApplication.class, args);
+	}
+
+}
